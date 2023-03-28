cade28cf3af6e373a734debc8dcded8a3c77734d
CalebW <jialewong98@gmail.com>
2023-03-28 16:51:25 +0800

    updated image track js

--- main.js
+++ main.js
@@ -1,66 +1,117 @@
 const track = document.getElementById('image-track');
 const image = track.children;
+const imageOrder = track.dataset.imageOrder;
 let isImageSelected = false;
 
-// // Select & Unselect Image
-// for (let i = 0; i < image.length; i++) {
-//   image[i].addEventListener('click', function (event) {
-//     // Use event.target to get the clicked image element
-//     const selectedImage = event.target;
-//     const imageOrder = Array.prototype.indexOf.call(image, selectedImage);
+// Select & Unselect Image
+for (let i = 0; i < image.length; i++) {
+  image[i].addEventListener('click', function (event) {
+    if (isImageSelected) return;
 
-//     // Add the "selected" class to the selected child element
-//     selectedImage.classList.add('selected');
+    // Use event.target to get the clicked image element
+    const selectedImage = event.target;
+    const imageOrder = Array.prototype.indexOf.call(image, selectedImage);
 
-//     // Remove the "selected" class from other image elements
-//     for (let i = 0; i < image.length; i++) {
-//       if (i !== imageOrder) {
-//         image[i].classList.remove('selected');
-//       }
-//     }
-//     // Modify the style of other child elements
-//     for (let i = 0; i < image.length; i++) {
-//       if (i !== imageOrder) {
-//         image[i].classList.add('others');
-//       } else {
-//         image[i].classList.remove('others');
-//       }
-//     }
+    track.dataset.imageOrder = imageOrder;
 
-//     // Set the selected state to true
-//     isImageSelected = true;
+    // Add the "selected" class to the selected child element
+    selectedImage.classList.add('selected');
 
-//     track.animate(
-//       {
-//         transform: `translate(0%, 0%)`,
-//       },
-//       { duration: 500, fill: 'forwards' }
-//     );
-//   });
-// }
+    // Remove the "selected" class from other image elements
+    for (let i = 0; i < image.length; i++) {
+      if (i !== imageOrder) {
+        image[i].classList.remove('selected');
+      }
+    }
+    // Modify the style of other child elements
+    for (let i = 0; i < image.length; i++) {
+      if (i !== imageOrder) {
+        image[i].classList.add('others');
+      } else {
+        image[i].classList.remove('others');
+      }
+    }
 
-// function deselectImage() {
-//   // Remove the "selected" and "others" classes from all image
-//   for (let i = 0; i < track.children.length; i++) {
-//     track.children[i].classList.remove('selected');
-//     track.children[i].classList.remove('others');
-//   }
+    // if (i !== imageOrder) {
+    //   image[i].animate(
+    //     {
+    //       // transform: `translate(${
+    //       //   i * (100 + 10) - track.dataset.imageOrder * (100 + 10)
+    //       // }%, 0%)`,
+    //       height: `0`,
+    //     },
+    //     { duration: 0, fill: 'forwards' }
+    //   );
+    // } else {
+    //   image[i].animate(
+    //     {
+    //       height: `100%`,
+    //       width: `100%`,
+    //       transform: `translate(0, 0)`,
+    //     },
+    //     { duration: 1200, fill: 'forwards' }
+    //   );
+    // }
 
-//   track.animate(
-//     {
-//       transform: `translate(${track.dataset.percentage}%, 0%)`,
-//     },
-//     { duration: 1200, fill: 'forwards' }
-//   );
+    if (i === imageOrder) {
+      image[i].animate(
+        {
+          transform: `translate(0%, 0%)`,
+        },
+        { duration: 1500, fill: 'forwards' }
+      );
+    }
 
-//   isImageSelected = false;
-// }
+    // image[i].animate(
+    //   {
+    //     transform: `translate(${
+    //       i * (100 + 10) - (image.length - 1) * ((-nextPercentage / 100) * 110)
+    //     }%, 0%)`,
+    //     objectPosition: `${100 + nextPercentage}% center`,
+    //   },
+    //   { duration: 1200, fill: 'forwards' }
+    // );
+
+    // Set the selected state to true
+    isImageSelected = true;
+  });
+}
+
+function deselectImage() {
+  // Remove the "selected" and "others" classes from all image
+  for (let i = 0; i < image.length; i++) {
+    console.log(i * (100 + 10) - (track.dataset.imageOrder + 1) * (100 + 10));
+    image[i].classList.remove('selected');
+    image[i].classList.remove('others');
+
+    if (i !== imageOrder) {
+      image[i].animate(
+        {
+          transform: `translate(${
+            i * (100 + 10) - track.dataset.imageOrder * (100 + 10)
+          }%, 0%)`,
+        },
+        { duration: 0, fill: 'forwards' }
+      );
+    } else {
+      image[i].animate(
+        {
+          transform: `translate(${
+            i * (100 + 10) - track.dataset.imageOrder * (100 + 10)
+          }%, 0%)`,
+        },
+        { duration: 1200, fill: 'forwards' }
+      );
+    }
+  }
+
+  isImageSelected = false;
+}
 
 // Mouse Wheel Scroll
 function scrollImage(e) {
   const wheelDelta = e.deltaY,
     maxDelta = window.innerWidth;
-  // maxWidth = (track.children.length - 1) * 110;
 
   const percentage = (wheelDelta / maxDelta) * -100,
     nextPercentageUnconstrained =
@@ -71,24 +122,17 @@ function scrollImage(e) {
 
   track.dataset.prevPercentage = track.dataset.percentage;
 
-  for (let i = 0; i < track.children.length; i++) {
-    // console.log(i * 110 + 220);
+  for (let i = 0; i < image.length; i++) {
     image[i].animate(
       {
-        transform: `translate(${i * 110 + i * 110 - 220}%, 0%)`,
+        transform: `translate(${
+          i * (100 + 10) - (image.length - 1) * ((-nextPercentage / 100) * 110)
+        }%, 0%)`,
+        objectPosition: `${100 + nextPercentage}% center`,
       },
       { duration: 1200, fill: 'forwards' }
     );
   }
-
-  // for (const image of track.getElementsByClassName('image')) {
-  //   image.animate(
-  //     {
-  //       objectPosition: `${100 + nextPercentage}% center`,
-  //     },
-  //     { duration: 1200, fill: 'forwards' }
-  //   );
-  // }
 }
 
 // Mouse Drag Scroll
@@ -105,7 +149,7 @@ function dragImage(e) {
   if (track.dataset.mouseDownAt === '0') return;
 
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth;
+    maxDelta = window.innerWidth / 2;
 
   const percentage = (mouseDelta / maxDelta) * -100,
     nextPercentageUnconstrained =
@@ -114,21 +158,17 @@ function dragImage(e) {
 
   track.dataset.percentage = nextPercentage;
 
-  track.animate(
-    {
-      transform: `translate(${nextPercentage}%, 0%)`,
-    },
-    { duration: 1200, fill: 'forwards' }
-  );
-
-  // for (const image of track.getElementsByClassName('image')) {
-  //   image.animate(
-  //     {
-  //       objectPosition: `${100 + nextPercentage}% center`,
-  //     },
-  //     { duration: 1200, fill: 'forwards' }
-  //   );
-  // }
+  for (let i = 0; i < image.length; i++) {
+    image[i].animate(
+      {
+        transform: `translate(${
+          i * (100 + 10) - (image.length - 1) * ((-nextPercentage / 100) * 110)
+        }%, 0%)`,
+        objectPosition: `${100 + nextPercentage}% center`,
+      },
+      { duration: 1200, fill: 'forwards' }
+    );
+  }
 }
 
 // Event Listeners
